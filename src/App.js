bc6469734134bb8f9e80284efbc10b415016f43a
duchuy <dhuy221001@gmail.com>
2022-10-28 14:20:04 +0700

    Using Css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Heading from './components/Heading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+  	 	<div className="App" style={{ background :"red" }}> 
+			<h1>CSS</h1>
+			<Heading />
+    	</div>
+	);
 }
 
 export default App;
---
+++ src/components/Heading/index.js
@@ -0,0 +1,8 @@
+import './Heading.css'
+
+function Heading(){
+   return <h1 className='heading'>Heloo beloo </h1>
+}
+
+export default Heading
+
